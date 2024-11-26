d082c51a24ddb8559e2e942cbd312efbca6e6290
peleisaac <151077839+peleisaac@users.noreply.github.com>
2024-11-26 12:05:53 +0000

    Updated game.js with content from script.js

--- static/js/game.js
+++ static/js/game.js
@@ -180,3 +180,25 @@ window.onload = function() {
     createBoard('player-board', playerBoard, true);
     createBoard('computer-board', boardData, false);
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+    const popupOverlay = document.getElementById('popupOverlay');
+    const popup = document.getElementById('popup');
+    const closePopup = document.getElementById('closePopup');
+
+    function openPopup() {
+        popupOverlay.style.display = 'block';
+    }
+
+    function closePopupFunc() {
+        popupOverlay.style.display = 'none';
+    }
+
+    openPopup();
+    closePopup.addEventListener('click', closePopupFunc);
+    popupOverlay.addEventListener('click', function(event) {
+        if (event.target === popupOverlay) {
+            closePopupFunc();
+        }
+    });
+});
